632a9abc6c81af5db4952bc8c7104135f2ad98f0
Dado <Dado@Dado-PC.dimi.uniud.it.>
2015-03-09 18:22:46 +0100

    Initialization

---
+++ src/main/java/org/uniud/dcore/annotation/Annotator.java
@@ -0,0 +1,33 @@
+/*
+ *  This file is part of Distiller-CORE.
+
+    Distiller-CORE is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Distiller-CORE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Distiller-CORE.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.uniud.dcore.annotation;
+
+/**
+ * The Annotator interface sholud be implemented by any class that reads the whole text
+ * and produces an annotated version of it.
+ * 
+ * Part-Of-Speech taggers and Named-Entity-Recognition modules are examples of annotators.
+ * 
+ * 
+ * @author Dado
+ */
+public interface Annotator {
+    
+    
+    
+}
