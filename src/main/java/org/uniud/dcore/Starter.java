de54a79cc473c52b80a4685cbe85bf03e5ec3886
Marco <Marco@Uno.homenet.telecomitalia.it>
2015-05-27 23:31:51 +0200

    Better demo code

--- src/main/java/org/uniud/dcore/Starter.java
+++ src/main/java/org/uniud/dcore/Starter.java
@@ -31,6 +31,9 @@ import org.uniud.dcore.engine.Distiller;
  */
 public class Starter {
 
+    // The incipit of "The Idiot", by Fyodor Dostoyevsky.   
+    private static String dosto = "Towards the end of November, during a thaw, at nine o'clock one morning, a train on the Warsaw and Petersburg railway was approaching the latter city at full speed. The morning was so damp and misty that it was only with great difficulty that the day succeeded in breaking; and it was impossible to distinguish anything more than a few yards away from the carriage windows. Some of the passengers by this particular train were returning from abroad; but the third-class carriages were the best filled, chiefly with insignificant persons of various occupations and degrees, picked up at the different stations nearer town. All of them seemed weary, and most of them had sleepy eyes and a shivering expression, while their complexions generally appeared to have taken on the colour of the fog outside. When day dawned, two passengers in one of the third-class carriages found themselves opposite each other. Both were young fellows, both were rather poorly dressed, both had remarkable faces, and both were evidently anxious to start a conversation. If they had but known why, at this particular moment, they were both remarkable persons, they would undoubtedly have wondered at the strange chance which had set them down opposite to one another in a third-class carriage of the Warsaw Railway Company. ";
+    
     /**
      * The first three paragraphs of the javadoc page of SortedMap.
      *
@@ -64,39 +67,34 @@ public class Starter {
             + "Os fundamentos científicos para a engenharia de software envolvem o uso de modelos abstratos e precisos que permitem ao engenheiro especificar, projetar, implementar e manter sistemas de software, avaliando e garantindo suas qualidades. Além disso, a engenharia de software deve oferecer mecanismos para se planejar e gerenciar o processo de desenvolvimento de um sistema computacional.";
 
     public static void main(String[] args) {
-        // Extract information from the incipit of "The Idiot", by Fyodor Dostoyevsky.        
-        Distiller d = Distiller.getDefault();
-
-        DateTime zero = DateTime.now();
-
-        d.extract("Towards the end of November, during a thaw, at nine o'clock one morning, a train on the Warsaw and Petersburg railway was approaching the latter city at full speed. The morning was so damp and misty that it was only with great difficulty that the day succeeded in breaking; and it was impossible to distinguish anything more than a few yards away from the carriage windows. Some of the passengers by this particular train were returning from abroad; but the third-class carriages were the best filled, chiefly with insignificant persons of various occupations and degrees, picked up at the different stations nearer town. All of them seemed weary, and most of them had sleepy eyes and a shivering expression, while their complexions generally appeared to have taken on the colour of the fog outside. When day dawned, two passengers in one of the third-class carriages found themselves opposite each other. Both were young fellows, both were rather poorly dressed, both had remarkable faces, and both were evidently anxious to start a conversation. If they had but known why, at this particular moment, they were both remarkable persons, they would undoubtedly have wondered at the strange chance which had set them down opposite to one another in a third-class carriage of the Warsaw Railway Company. ");
-
-        DateTime one = DateTime.now();
-
-        // subsequent extractions are faster because only the first one loads definitions
-        //d.extract("One of them was a young fellow of about twenty-seven, not tall, with black curling hair, and small, grey, fiery eyes. His nose was broad and flat, and he had high cheek bones; his thin lips were constantly compressed into an impudent, ironical—it might almost be called a malicious—smile; but his forehead was high and well formed, and atoned for a good deal of the ugliness of the lower part of his face. A special feature of this physiognomy was its death-like pallor, which gave to the whole man an indescribably emaciated appearance in spite of his hard look, and at the same time a sort of passionate and suffering expression which did not harmonize with his impudent, sarcastic smile and keen, self-satisfied bearing. He wore a large fur—or rather astrachan—overcoat, which had kept him warm all night, while his neighbour had been obliged to bear the full severity of a Russian November night entirely unprepared. His wide sleeveless mantle with a large cape to it—the sort of cloak one sees upon travellers during the winter months in Switzerland or North Italy—was by no means adapted to the long cold journey through Russia, from Eydkuhnen to St. Petersburg. ");
-        d.extract(topGear);
-
-        DateTime two = DateTime.now();
-
-        d.extract(sortedMap);
-
-        DateTime three = DateTime.now();
-
-        d.extract(annaSavoia);
         
-        d.extract(softEngPT);
-
-        long firstExtraction = one.getMillis() - zero.getMillis();
-        long secondExtraction = two.getMillis() - one.getMillis();
-        long thirdExtraction = three.getMillis() - two.getMillis();
-
-        DecimalFormat df = new DecimalFormat("#.###");
-
+        String[] texts = new String[] {
+            dosto,
+            topGear,
+            sortedMap,
+            annaSavoia,
+            softEngPT
+        };
+             
+        Distiller d = Distiller.getDefault();
+        
+        DateTime[] times = new DateTime[texts.length + 1];
+        
+        times[0] = DateTime.now();
+        
+        for (int i = 0; i < texts.length; i++) {
+            d.extract(texts[i]);
+            times[i+1] = DateTime.now();        
+        }
+        
         System.out.println("** Extraction performance **");
-        System.out.println("First extraction\t " + df.format(firstExtraction / 1000.0) + " s (incl. initialization)");
-        System.out.println("Second extraction\t " + df.format(secondExtraction / 1000.0) + " s");
-        System.out.println("Third extraction\t " + df.format(thirdExtraction / 1000.0) + " s");
-
+        DecimalFormat df = new DecimalFormat("#.###");
+        
+        for (int i = 0; i < texts.length; i++) {
+            System.out.printf(
+                    "Iteration %d:\t %s seconds\n",
+                    i,
+                    df.format((times[i+1].getMillis() - times[i].getMillis()) / 1000.0));
+        }
     }
 }
