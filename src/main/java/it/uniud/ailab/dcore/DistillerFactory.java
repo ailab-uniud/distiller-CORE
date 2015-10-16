dc0b56803956dcdce9cf1a249f520cd6640098db
Marco Basaldella <basaldella.marco@gmail.com>
2015-10-16 12:24:15 +0200

    Add gram merger annotator Added in the factory at the correct step but commented, so that now is disabled in the default pipeline

--- src/main/java/it/uniud/ailab/dcore/DistillerFactory.java
+++ src/main/java/it/uniud/ailab/dcore/DistillerFactory.java
@@ -29,23 +29,23 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
  * A simple factory that generates the default Distiller configuration either
- * via XML configuration file or via Java code. We write this class also as
- * a tutorial for the users of this library, who can learn how to instantiate
- * the Distiller object studying this source code.
+ * via XML configuration file or via Java code. We write this class also as a
+ * tutorial for the users of this library, who can learn how to instantiate the
+ * Distiller object studying this source code.
  *
  * @author Marco Basaldella
  */
 public class DistillerFactory {
-    
+
     /**
-     * Instantiates a Distiller object using the default XML configuration; 
-     * if it's not available, uses the safer (but less precise) code configuration,
+     * Instantiates a Distiller object using the default XML configuration; if
+     * it's not available, uses the safer (but less precise) code configuration,
      * which excludes TagMe and inference from the distillation process.
      *
      * @return a Distiller ready to work.
      */
     public static Distiller getDefault() {
-        
+
         // While the code under this comment may be ugly, it works.
         //
         // It tries to load the default XML config. If the load fails, throws
@@ -58,9 +58,8 @@ public class DistillerFactory {
         // Otherwise, the exception is re-thrown, so that the developer can
         // handle the errors in the config file, which are the other most likely
         // cause of failure of configuration loading falure.
-        
         try {
-            return getDefaultXML();            
+            return getDefaultXML();
         } catch (BeanDefinitionStoreException bsde) {
             try {
                 throw bsde.getCause();
@@ -72,36 +71,36 @@ public class DistillerFactory {
                 return getDefaultCode();
             } catch (Throwable te) {
                 throw bsde;
-            }            
-        }        
+            }
+        }
     }
-    
+
     /**
-     * Instantiates a Distiller object using the default evaluation configuration.
+     * Instantiates a Distiller object using the default evaluation
+     * configuration.
      *
      * @return a Distiller ready to work.
      */
     public static Distiller getDefaultEval() {
-        
-        
+
         try {
             ApplicationContext context = new ClassPathXmlApplicationContext("eval.xml");
-            return (Distiller) context.getBean("distiller");        
+            return (Distiller) context.getBean("distiller");
         } catch (BeanDefinitionStoreException bsde) {
             try {
                 throw bsde.getCause();
             } catch (IOException ioe) {
                 // the configuration file does not exist or is not accessible:
-                throw new RuntimeException("FATAL: Impossible to load the default evaluation pipeline.",ioe);
+                throw new RuntimeException("FATAL: Impossible to load the default evaluation pipeline.", ioe);
             } catch (Throwable te) {
                 throw bsde;
-            }            
-        }        
+            }
+        }
     }
-    
+
     /**
      * Instantiates a Distiller object using the specified configuration and
-     * returns it. 
+     * returns it.
      *
      * @param path the path where the config file is located
      * @return a Distiller ready to work.
@@ -109,11 +108,10 @@ public class DistillerFactory {
     public static Distiller loadFromXML(File path) {
         // We add the file:// thing before because, for some ??? reason, 
         // the Spring Framework decided that all paths are relative paths.
-        
+
         // So, we get the file, retrieve is absolute path, and then add the
         // file:// prefix to be sure that the Spring Frameworks treats 
         // all paths as absolute paths. 
-        
         // This is less problematic, because the Java platform will handle the
         // File and produce its absolute path, even if it has been created
         // using a relative one.
@@ -121,11 +119,11 @@ public class DistillerFactory {
                 "file://" + path.getAbsolutePath());
         return (Distiller) context.getBean("distiller");
     }
-    
+
     /**
      * Instantiates a Distiller object using the default configuration and
-     * returns it. Please note that you should create a config.xml file 
-     * and copy the content of default.xml inside it to get the framework to work.
+     * returns it. Please note that you should create a config.xml file and copy
+     * the content of default.xml inside it to get the framework to work.
      *
      * @return a Distiller ready to work.
      */
@@ -133,65 +131,63 @@ public class DistillerFactory {
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
         return (Distiller) context.getBean("distiller");
     }
-    
+
     public static Distiller getDefaultCode() {
         Distiller d = new Distiller();
-        
+
         // set the language detector tool
         d.setLanguageDetector(new CybozuLanguageDetectorAnnotator());
-        
+
         // build the pipeline
         Pipeline p = new Pipeline();
         // split the text
         p.addAnnotator(new OpenNlpBootstrapperAnnotator());
         // add wikipedia tags to tokens
-        
+
         // Uncomment the lines below to use the TagMe service
-        
         // TagMeTokenAnnotator tagme = new TagMeTokenAnnotator();        
         // tagme.setApiKey("INSERT KEY HERE");        
         // p.addAnnotator(tagme);
-        
         // generate ngrams
         p.addAnnotator(new SimpleNGramGeneratorAnnotator());
-        
+
         // remove stopwords
         p.addAnnotator(new StopwordSimpleFilterAnnotator());
-        
+
         // annotate ngrams
         p.addAnnotator(new StatisticalAnnotator());
-        
+
         // Uncomment to use TagMe
         // p.addAnnotator(new TagMeGramAnnotator());
-        
         // Uncomment to use the emotional intensity annotator.
         // This way you'll see how different annotators lead to different
         // keyphrases detection
         // p.addAnnotator(new SyuzhetAnnotator());
-        
-        
         // evaluate ngram features        
         LinearEvaluatorAnnotator evaluator = new LinearEvaluatorAnnotator();
         evaluator.addWeight(StatisticalAnnotator.DEPTH, 0.15);
         evaluator.addWeight(StatisticalAnnotator.HEIGHT, 0.25);
         evaluator.addWeight(StatisticalAnnotator.LIFESPAN, 0.1);
-        evaluator.addWeight(StatisticalAnnotator.FREQUENCY_SENTENCE, 0.1);  
-        evaluator.addWeight(GenericNGramGeneratorAnnotator.NOUNVALUE,0.3);
+        evaluator.addWeight(StatisticalAnnotator.FREQUENCY_SENTENCE, 0.1);
+        evaluator.addWeight(GenericNGramGeneratorAnnotator.NOUNVALUE, 0.3);
         evaluator.addWeight(GenericWikipediaAnnotator.WIKIFLAG, 0.1);
-       
+
         p.addAnnotator(evaluator);
-        
+
         // Uncomment the line below to infer concepts.
         // Watch out: the inference process sends lots of requests to Wikipedia, 
         // so it significantly slows down the process
         // p.addAnnotator(new WikipediaInferenceAnnotator());
         // filter results
         p.addAnnotator(new SkylineGramFilterAnnotator());
-                
+
+        // remove redundant grams
+        //p.addAnnotator(new GramMergerAnnotator());
+        
         d.addPipeline(Locale.ENGLISH, p);
         d.addPipeline(Locale.ITALIAN, p);
-        
+
         return d;
     }
-    
+
 }
